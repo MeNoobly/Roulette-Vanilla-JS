bf4d3932e38b62cbdd2b8d16a707070f9296159b
Nick <84956743974@mail.ru>
2022-11-05 23:25:09 +0300

    continue do roulette

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@
     const roulette = {
         
         money: 1000,
-
+        amountBet: 0,
+        bet: [],
         values: {
             values: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
             wheelValues: [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26],
@@ -25,14 +26,29 @@
             return number + " " + color;
         },
 
-        setBet() {
+        setBet(context, domElement) {
+          return function () {
             let rouletteBetInput = document.querySelector(".roulette__bet");
             let rouletteBetP = document.querySelector(".roulette__money-bet");
             let rouletteMoneyP = document.querySelector(".roulette__money");
-            rouletteBetP.textContent = `Ставка: ${rouletteBetInput.value}`;
-            this.money -= +rouletteBetInput.value;
-            rouletteMoneyP.textContent = this.money;
+            let rouletteResult = document.querySelector(".roulette__result");
+            let rouletteTableElement = document.createElement("p");
+
+            rouletteBetP.textContent = `Общая сумма ставки: ${context.amountBet + Number(rouletteBetInput.value)}`;
+            context.amountBet += Number(rouletteBetInput.value);
+            context.money -= +rouletteBetInput.value;
+            rouletteMoneyP.textContent = context.money;
+
+            context.bet.push(
+              {
+                [domElement.id]: +rouletteBetInput.value
+              });
+
+            rouletteTableElement.textContent = `Ставка на ${domElement.id}, сумма ставки ${rouletteBetInput.value}`;
             rouletteBetInput.value = "";
+            rouletteResult.classList.add("roulette__table-element");
+            rouletteResult.append(rouletteTableElement);
+          };
         },
 
         createRouletteApp() {
@@ -43,13 +59,14 @@
             let li, div, p;
 
             p = document.querySelector(".roulette__div-zero");
-            p.addEventListener("click", this.setBet.bind(this));
-
-            for (let i = 0; i < 36; i++) {
+            p.addEventListener("click", this.setBet(this, p));
+            p.id = "0";
+            for (let i = 1; i < 37; i++) {
                 div = document.createElement("div");
                 li = document.createElement("li");
-                li.textContent = this.values.values[i];
-                li.addEventListener("click", this.setBet.bind(this));
+                li.textContent = this.values.values[i - 1];
+                li.addEventListener("click", this.setBet(this, li));
+                li.id = `${this.values.values[i - 1]}`;
                 div.classList.add("roulette__green-background");
                 li.classList.add("roulette__item");
                 div.append(li);
@@ -67,8 +84,6 @@
             }
             rouletteMoneyP.textContent = this.money;
         },
-
-
     };
 
     roulette.createRouletteApp();
@@ -118,7 +133,7 @@
     // переменная для текстовых подписей
     let prizeNodes;
 
-    // расставляем текст по секторам
+    // расставляем текст по секторамs
     const createPrizeNodes = () => {
       // обрабатываем каждую подпись
       prizes.forEach(({ text, color, reaction }, i) => {
@@ -137,6 +152,14 @@
         );
       });
     };
+
+    function winOrLoose(position) {
+
+      let setValues = roulette.bet.map(item => Object.keys(item)[0]);
+      console.log(setValues);
+      console.log(roulette.bet);
+      console.log(position);
+    }
 
     // рисуем разноцветные секторы
     const createConicGradient = () => {
@@ -180,7 +203,9 @@
       const b = values[1];  
       let rad = Math.atan2(b, a);
       
-      if (rad < 0) rad += (2 * Math.PI);
+      if (rad < 0) {
+        rad += (2 * Math.PI);
+      }
       
       const angle = Math.round(rad * (180 / Math.PI));
       const slice = Math.floor(angle / prizeSlice);
@@ -203,6 +228,7 @@
     const selectPrize = () => {
       const selected = Math.floor(rotation / prizeSlice);
       prizeNodes[selected].classList.add(selectedClass);
+      winOrLoose(Array.from(prizeNodes[selected].childNodes)[1].textContent);
     };
 
     // отслеживаем нажатие на кнопку
@@ -220,7 +246,7 @@
       // возвращаем язычок в горизонтальную позицию
       ticker.style.animation = "none";
       // запускаем анимацию вращение
-      runTickerAnimation();
+      runTickerAnimation(); 
     });
 
     // отслеживаем, когда закончилась анимация вращения колеса
