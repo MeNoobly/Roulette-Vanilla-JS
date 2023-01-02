ff154f0f998b3d2c803b8517529ee4cbbb8da9f6
Nick <84956743974@mail.ru>
2023-01-02 22:50:16 +0300

    add js

--- js/black-jack/script.js
+++ js/black-jack/script.js
@@ -1,1 +1,23 @@
-﻿
+﻿"use strict";
+
+const user = {
+    money: 1000
+};
+
+let startButton = document.querySelector(".start");
+
+startButton.addEventListener("click", start);
+
+function start() {
+    let arrayOfCards = document.querySelectorAll(".card");
+    let id = 0;
+    let timerId = setInterval(() => {
+        if (id === arrayOfCards.length - 1) {
+            clearInterval(timerId);
+        }
+
+        arrayOfCards[id].classList.remove("opacity-off");
+        arrayOfCards[id].classList.add("opacity-on");
+        id++;
+    }, 500);
+}
